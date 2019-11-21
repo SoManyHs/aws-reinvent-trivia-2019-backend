56e894e4f27bf70b1fe40e1f8cfcb8ea6951c0d9
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-11-21 13:26:30 -0800

    Revert "Add OneBox stage"
    This reverts commit 6529909baa5e6dcff21a46cfeedaae383c44a28b.

--- trivia-backend/infra/codedeploy-blue-green/setup.sh
+++ trivia-backend/infra/codedeploy-blue-green/setup.sh
@@ -33,58 +33,36 @@ export AWS_REGION=us-east-1
 
 node produce-config.js -g test -s TriviaBackendTest -h TriviaBackendHooksTest
 
-node produce-config.js -g onebox -s TriviaBackendProd -h TriviaBackendHooksProd
-
 node produce-config.js -g prod -s TriviaBackendProd -h TriviaBackendHooksProd
 
 # Create ECS resources
 
 ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text --region us-east-1`
 
-sed -i'.bak' "s|<PLACEHOLDER>|$ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reinvent-trivia-backend:latest|g" build/task-definition-test.json build/task-definition-onebox.json build/task-definition-prod.json
+sed -i.bak "s|<PLACEHOLDER>|$ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reinvent-trivia-backend:latest|g" build/task-definition-test.json build/task-definition-prod.json
 
 aws ecs create-cluster --region us-east-1 --cluster-name default
 
 aws ecs register-task-definition --region us-east-1 --cli-input-json file://build/task-definition-test.json
 
-aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-test --cli-input-json file://build/service-definition-test.json || \
-  aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-test --cli-input-json file://build/service-definition-test-alt.json || \
-  true
-
-aws ecs register-task-definition --region us-east-1 --cli-input-json file://build/task-definition-onebox.json
-
-aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-onebox --desired-count 1 --cli-input-json file://build/service-definition-onebox.json || \
-  aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-onebox --desired-count 1 --cli-input-json file://build/service-definition-onebox-alt.json || \
-  true
+aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-test --cli-input-json file://build/service-definition-test.json
 
 aws ecs register-task-definition --region us-east-1 --cli-input-json file://build/task-definition-prod.json
 
-aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-prod --cli-input-json file://build/service-definition-prod.json || \
-  aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-prod --cli-input-json file://build/service-definition-prod-alt.json || \
-  true
+aws ecs create-service --region us-east-1 --service-name trivia-demo-backend-prod --cli-input-json file://build/service-definition-prod.json
 
 # Create CodeDeploy resources
 
-set +e
-
 aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-demo-backend-test --compute-platform ECS
-
-aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-demo-backend-onebox --compute-platform ECS
 
 aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-demo-backend-prod --compute-platform ECS
 
 aws deploy create-deployment-group --region us-east-1 --deployment-group-name DgpECS-default-trivia-demo-backend-test --cli-input-json file://build/deployment-group-test.json
 
-aws deploy create-deployment-group --region us-east-1 --deployment-group-name DgpECS-default-trivia-demo-backend-onebox --cli-input-json file://build/deployment-group-onebox.json
-
 aws deploy create-deployment-group --region us-east-1 --deployment-group-name DgpECS-default-trivia-demo-backend-prod --cli-input-json file://build/deployment-group-prod.json
-
-set -e
 
 # Start deployment
 
 aws ecs deploy --region us-east-1 --service trivia-demo-backend-test --task-definition build/task-definition-test.json --codedeploy-appspec build/appspec-test.json
-
-aws ecs deploy --region us-east-1 --service trivia-demo-backend-onebox --task-definition build/task-definition-onebox.json --codedeploy-appspec build/appspec-onebox.json
 
 aws ecs deploy --region us-east-1 --service trivia-demo-backend-prod --task-definition build/task-definition-prod.json --codedeploy-appspec build/appspec-prod.json
